2ceb49244b8243593c0a52d89574dbc681bdc3c8
erwinjames <manugasewinjames@gmail.com>
2024-04-15 15:40:16 +0800

    updating new pages room and roomrates

---
+++ js/custom_script.js
@@ -0,0 +1,64 @@
+/*  ---------------------------------------------------
+    Template Name: Hotel Template
+    Description: Hotel HTML Template
+    Author: colorlib
+    Author URI: https://www.colorlib.com/
+    Version: 1.0
+    Created: colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+    /*------------------
+        Room Pic Slider
+    --------------------*/
+    $(".room-pic-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+        smartSpeed: 800,
+        autoplay: false,
+    });
+
+    /*-------------------
+		Quantity change
+	--------------------- */
+    var proQty = $('.pro-qty');
+    proQty.prepend('<span class="dec qtybtn">-</span>');
+    proQty.append('<span class="inc qtybtn">+</span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.pop-up').magnificPopup({
+        type: 'iframe'
+    });
+
+})(jQuery);
